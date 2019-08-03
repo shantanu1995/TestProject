a1d166181d16984bc13c0ffbced367aa8998dd4e
shantanu1995 <shantanuchaterjee9@gmail.com>
2019-08-04 00:58:24 +0200

    change version 9

--- app.go
+++ app.go
@@ -74,6 +74,7 @@ func FindUserEndpoint(w http.ResponseWriter, r *http.Request) {
 
 		dateStamp, _ = time.Parse("2012-02-01", exercise[i].Date); 
 		dateStamp2, _ = time.Parse("2012-02-01", from); 
+		fmt.Printf(i)
 		if !dateStamp.After(dateStamp2) {
 
 			exercise = remove(exercise,i)
