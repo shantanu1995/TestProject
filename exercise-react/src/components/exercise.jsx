679d8075c6765cf2eed816d92731b9d75a028f37
shantanu1995 <shantanuchaterjee9@gmail.com>
2019-08-04 23:12:16 +0200

    change version 9

--- exercise-react/src/components/exercise.jsx
+++ exercise-react/src/components/exercise.jsx
@@ -2,8 +2,15 @@ import React, { Component } from "react";
 class Exercise extends Component {
   constructor(props) {
     super(props);
-    this.state = { username: "" };
+    this.state = {
+      username: "",
+      id: "",
+      description: "",
+      duration: "",
+      date: ""
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleSubmit1 = this.handleSubmit1.bind(this);
   }
   handleSubmit(event) {
     fetch("http://localhost:3000/api/exercise/new-user", {
@@ -18,7 +25,14 @@ class Exercise extends Component {
     })
       .then(res => res.json())
       .then(json => {
-        alert(JSON.stringify(json));
+        var winPrint = window.open(
+          "",
+          "",
+          "left=0,top=0,width=800,height=600,toolbar=0,scrollbars=0,status=0"
+        );
+        winPrint.document.write(JSON.stringify(json));
+        winPrint.document.close();
+        winPrint.focus();
       });
     event.preventDefault();
   }
@@ -39,7 +53,14 @@ class Exercise extends Component {
     })
       .then(res => res.json())
       .then(json => {
-        alert(JSON.stringify(json));
+        var winPrint = window.open(
+          "",
+          "",
+          "left=0,top=0,width=800,height=600,toolbar=0,scrollbars=0,status=0"
+        );
+        winPrint.document.write(JSON.stringify(json));
+        winPrint.document.close();
+        winPrint.focus();
       });
     event.preventDefault();
   }
@@ -52,54 +73,57 @@ class Exercise extends Component {
     return (
       <div className="container">
         <h1>Exercise tracker</h1>
-        <form
-          name="usercreate"
-          className="form-create-user"
-          onSubmit={this.handleSubmit}
-        >
-          <h3>Create a New User</h3>
-          <p>
-            <code>POST /api/exercise/new-user</code>
-          </p>
-          <input
-            type="text"
-            ref="username"
-            name="username"
-            placeholder="username"
-            onChange={this.handleChange}
-          />
-          <input type="submit" value="Submit" />
-        </form>
-        <form
-          name="usercreate1"
-          className="form-create-user1"
-          onSubmit={this.handleSubmit1}
-        >
-          <h3>Add exercises</h3>
-          <p>
-            <code>POST /api/exercise/add</code>
-          </p>
-          <input ref="uid" type="text" name="userId" placeholder="userId*" />
-          <input
-            ref="desc"
-            type="text"
-            name="description"
-            placeholder="description*"
-          />
-          <input
-            ref="dur"
-            type="text"
-            name="duration"
-            placeholder="duration* (mins.)"
-          />
-          <input
-            ref="dat"
-            type="text"
-            name="date"
-            placeholder="date (yyyy-mm-dd)"
-          />
-          <input type="submit" value="Submit" />
-        </form>
+        <div>
+          <form
+            name="usercreate"
+            className="form-create-user"
+            onSubmit={this.handleSubmit}
+          >
+            <h3>Create a New User</h3>
+            <p>
+              <code>POST /api/exercise/new-user</code>
+            </p>
+            <input
+              type="text"
+              ref="username"
+              name="username"
+              placeholder="username"
+              onChange={this.handleChange}
+            />
+            <input type="submit" value="Submit" />
+          </form>
+
+          <form
+            name="usercreate1"
+            className="form-create-user1"
+            onSubmit={this.handleSubmit1}
+          >
+            <h3>Add exercises</h3>
+            <p>
+              <code>POST /api/exercise/add</code>
+            </p>
+            <input ref="uid" type="text" name="userId" placeholder="userId*" />
+            <input
+              ref="desc"
+              type="text"
+              name="description"
+              placeholder="description*"
+            />
+            <input
+              ref="dur"
+              type="text"
+              name="duration"
+              placeholder="duration* (mins.)"
+            />
+            <input
+              ref="dat"
+              type="text"
+              name="date"
+              placeholder="date (yyyy-mm-dd)"
+            />
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
         <p>
           <strong>GET users's exercise log: </strong>
           <code>
